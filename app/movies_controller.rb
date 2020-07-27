78251d5c8866d8fe7b0e9315108c78030b2a5964
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-27 02:08:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -39,7 +39,7 @@ def can_be_created_in_a_block(args = {:title =>  "Home Alone", :release_date => 
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.title.first
+  Movie.first.title(1)
 end
 
 def can_get_the_last_item_in_the_database
